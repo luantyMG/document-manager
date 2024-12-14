45f0cbe0b7207b7400105e08a96d0bc0a4402854
Luis Montejo <luismontejo90@gmail.com>
2024-12-14 16:23:36 -0600

    API get all

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -4,6 +4,7 @@ import { connection } from "../../../utils/database";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method, body } = req;
+
   switch (method) {
     case "GET":
       try {
@@ -16,15 +17,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     case "POST":
       try {
-        const query =
-          "INSERT INTO tareas (title, description, status) VALUES ($1, $2, $3) RETURNING *";
-        const values = [body.title, body.description, body.status];
+        const { creador_id, asunto, descripcion,prioridad, estatus } = body;
+
+        const query = "INSERT INTO tareas (creador_id, asunto,descripcion,prioridad,estatus) VALUES ($1,$2,$3,$4,$5) RETURNING *";
+        const values = [creador_id, asunto, descripcion, prioridad, estatus];
         const result = await connection.query(query, values);
         console.log(result);
-        return res.status(200).json("Task created");
+        return res.status(200).json(result.rows[0]);  
       } catch (error) {
         console.log(error);
       }
+
     default:
       return res.status(405).json({ message: "Method not allowed" });
   }
