cc25a8029237f88ca48b216ab3c1a37838775f8a
Luis Montejo <luismontejo90@gmail.com>
2024-12-16 11:54:38 -0600

    Actualizando Botones Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null; // Si el modal no está abierto, no renderizamos nada
 
+  
+
   return (
     <>
-      <div className="modal modal-open">
+      <div className="modal modal-open max-h-max">
         <div className="modal-box">
           <h3 className="text-lg font-bold">Redactar un mensaje</h3>
           <form>
@@ -34,9 +36,37 @@ const Modal = ({ isOpen, onClose }) => {
               ></textarea>
             </div>
           </form>
-          <div className="modal-action">
-            <button className="btn" onClick={onClose}>Cerrar</button>
-            <button className="btn btn-primary">Enviar</button>
+          <div className="contenedorfile">
+            <input type="file" className="file-input w-full py-2 " />
+          </div>
+          <div className="flex items-center space-x-4">
+            <label className="text-sm text-gray-700">Marcar como urgente</label>
+            <input
+              type="checkbox"
+              className="toggle toggle-error"
+              defaultChecked={false}
+            />
+          </div>
+          <div className="modal-action justify-between">
+            <button className="btn btn-outline btn-success py-1">
+              Vista Previa
+            </button>
+          </div>
+          <div className="modal-action justify-between">
+            <div>
+              <button className="btn btn-outline btn-error">Firmar</button>
+            </div>
+            <div className="flex items-center space-x-4">
+              <button
+                className="px-4 py-2 bg-transparent text-blue-500 font-bold border-2 border-blue-500 rounded-lg text-sm hover:bg-blue-500 hover:text-black hover:border-blue-500 transition-colors duration-200 ease-in-out"
+                onClick={onClose}
+              >
+                Cerrar
+              </button>
+              <button className="px-5 py-3 bg-blue-500 text-black font-bold rounded-md text-xs border-2 border-blue-500 hover:bg-blue-600 hover:border-blue-600 hover:text-white transition-all duration-300 ease-in-out shadow-md hover:shadow-lg">
+                Enviar
+              </button>
+            </div>
           </div>
         </div>
       </div>
